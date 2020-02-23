6cbead9060cd029d8a1c488f2880d3895225874a
Eduard-Dumitrescu <eduard.dumitrescu94@gmail.com>
2020-02-23 20:58:01 +0200

    comment route_generator cause it was somehow being used

--- lib/utils/route_generator.dart
+++ lib/utils/route_generator.dart
@@ -1,40 +1,44 @@
-import 'package:ciocio_team_generator/ui/home_page.dart';
-import 'package:ciocio_team_generator/ui/players_page.dart';
-import 'package:flutter/material.dart';
-
-// will maybe use later
-class RouteGenerator {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    // Getting arguments passed in while calling Navigator.pushNamed
-    final args = settings.arguments;
-
-    // hack to cache pages and not make keyboard rebuild them
-    final HomePage _homePage = HomePage();
-    const PlayersPage _playersPage = PlayersPage();
-
-    switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) => _homePage);
-      case '/players':
-        return MaterialPageRoute(
-          builder: (_) => _playersPage,
-        );
-      default:
-        // If there is no such named route in the switch statement, e.g. /third
-        return _errorRoute();
-    }
-  }
-
-  static Route<dynamic> _errorRoute() {
-    return MaterialPageRoute(builder: (_) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Error'),
-        ),
-        body: Center(
-          child: Text('ERROR'),
-        ),
-      );
-    });
-  }
-}
+//import 'package:ciocio_team_generator/ui/contact_page.dart';
+//import 'package:ciocio_team_generator/ui/home_page.dart';
+//import 'package:ciocio_team_generator/ui/players_page.dart';
+//import 'package:flutter/material.dart';
+//
+//// will maybe use later
+//class RouteGenerator {
+//  static Route<dynamic> generateRoute(RouteSettings settings) {
+//    // Getting arguments passed in while calling Navigator.pushNamed
+//    final args = settings.arguments;
+//
+//    // hack to cache pages and not make keyboard rebuild them
+//    final HomePage _homePage = HomePage(
+//      title: "whatever",
+//    );
+//    final PlayersPage _playersPage = PlayersPage();
+//    final ContactPage _contactPage = ContactPage();
+//
+//    switch (settings.name) {
+//      case '/':
+//        return MaterialPageRoute(builder: (_) => _homePage);
+//      case '/players':
+//        return MaterialPageRoute(builder: (_) => _playersPage);
+//      case '/contact':
+//        return MaterialPageRoute(builder: (_) => _contactPage);
+//      default:
+//        // If there is no such named route in the switch statement, e.g. /third
+//        return _errorRoute();
+//    }
+//  }
+//
+//  static Route<dynamic> _errorRoute() {
+//    return MaterialPageRoute(builder: (_) {
+//      return Scaffold(
+//        appBar: AppBar(
+//          title: Text('Error'),
+//        ),
+//        body: Center(
+//          child: Text('ERROR'),
+//        ),
+//      );
+//    });
+//  }
+//}
