549eaaa6d9494aa33d3559f43439174b7d3f4715
Eduard-Dumitrescu <eduard.dumitrescu94@gmail.com>
2020-03-08 21:56:10 +0200

    fixed utils for ios(maybe)

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,24 +1,27 @@
 import 'dart:core';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+//need more research
 class Utils {
   static double deviceWidth(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
-    return Platform.isIOS ? deviceWidth / 2 : deviceWidth;
+    //return Platform.isIOS ? deviceWidth / 2 : deviceWidth;
+    return deviceWidth;
   }
 
   static double deviceHeight(BuildContext context) {
     final double deviceHeight = MediaQuery.of(context).size.height;
-    return Platform.isIOS ? deviceHeight / 2 : deviceHeight;
+    //return Platform.isIOS ? deviceHeight / 2 : deviceHeight;
+    return deviceHeight;
   }
 
   static double deviceHeightWithoutAppBar(BuildContext context) {
     final double deviceHeight =
         MediaQuery.of(context).size.height - kToolbarHeight;
-    return Platform.isIOS ? deviceHeight / 2 : deviceHeight;
+    //return Platform.isIOS ? deviceHeight / 2 : deviceHeight;
+    return deviceHeight;
   }
 
   static bool isKeyboardHidden(BuildContext context) {
